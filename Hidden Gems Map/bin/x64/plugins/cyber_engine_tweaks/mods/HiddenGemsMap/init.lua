afd76035a9b027ec3dfad67a6aad723d9df77053
Mad Cat MKII <mad.cat.mkii.br@gmail.com>
2024-07-31 21:04:54 -0300

    Small database corrections

--- Hidden Gems Map/bin/x64/plugins/cyber_engine_tweaks/mods/HiddenGemsMap/init.lua
+++ Hidden Gems Map/bin/x64/plugins/cyber_engine_tweaks/mods/HiddenGemsMap/init.lua
@@ -8,7 +8,7 @@ local Utils = require('modules/Utils')
 local Vars = require('modules/Vars')
 
 local HiddenGemsMap = {
-    version = '1.3.9',
+    version = '1.3.10',
     cet = 1.32,
     filename = 'settings.json',
     logname = 'console.log',
